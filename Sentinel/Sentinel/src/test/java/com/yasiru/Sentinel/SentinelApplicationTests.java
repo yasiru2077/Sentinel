23aec31d5a89d1b176ec069c20894dcaaeecd253
yasiru2077 <yasiruudana1999@gmail.com>
2026-04-02 15:41:48 +0530

    initializing the project

---
+++ Sentinel/Sentinel/src/test/java/com/yasiru/Sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasiru.Sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
